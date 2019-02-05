be6c37696072897ec8f963b3fba0d62ab57496cc
chharish769 <chharish769@gmail.com>
2019-02-05 01:04:32 +0000

    SE_HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,73 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+   arr_value=arr.count
+  
+  if arr_value==0 
+    return 0
+  elsif arr_value==1
+    return arr[0]
+  else
+    arr.sort!
+    #arr.sort! {|a,b| a<=>b}
+    return (arr[-1]+arr[-2])
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr_value=arr.count
+  if arr_value==0 
+    return false
+  elsif arr_value==1
+    return false
+  else
+  !!arr.uniq.combination(2).detect { |a, b| a + b == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  
+  
+  #if /^[^aeiou\W]/i.match(s)== nil
+    #return false
+  #else
+   # return true
+  
+  #end
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == "0"
+    return true
+   else
+    /^[10]*00$/.match(s) != nil
+  end
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price 
+      def initialize(isbn,price)
+        @isbn= isbn
+        @price=price
+        raise ArgumentError,
+              "Enter the correct values for arguements" if isbn.empty? or price<=0
+      end
+      def price_as_string
+        #number_to_currency(@price)
+        return "$#{'%0.2f' % @price}"
+      end 
 end
